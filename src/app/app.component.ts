a0167939c8399336752ddbb3e0f450f8238541dc
Ram Singh <ram.singh.ctr@sabre.com>
2020-02-03 11:12:03 +0530

    Added auto login on app resume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import {Plugins, Capacitor} from '@capacitor/core';
+import {Plugins, AppState, Capacitor} from '@capacitor/core';
 import { AuthService } from './auth/auth.service';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -28,6 +29,7 @@ export class AppComponent implements OnInit, OnDestroy {
       }
       this.previousAuthState = isAuth;
     });
+    Plugins.App.addListener('appStateChange', this.checkAuthOnResume.bind(this));
   }
   initializeApp() {
     this.platform.ready().then(() => {
@@ -41,6 +43,18 @@ export class AppComponent implements OnInit, OnDestroy {
   onLogout() {
     this.authService.logout();
     this.router.navigateByUrl('/auth');
+  }
+  private checkAuthOnResume(state: AppState) {
+    if (state.isActive) {
+      this.authService
+        .autoLogin()
+        .pipe(take(1))
+        .subscribe(success => {
+          if (!success) {
+            this.onLogout();
+          }
+        });
+    }
   }
   ngOnDestroy() {
     if (this.authSub) {
